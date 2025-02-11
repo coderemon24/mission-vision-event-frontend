cb35daf9734dc78ce48b5662ec6c660907501097
coderemon24 <mdemonahmed2021@gmail.com>
2025-02-11 22:55:31 +0600

    event managment system frontend

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+$('.main-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
+      
