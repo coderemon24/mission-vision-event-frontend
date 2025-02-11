ea9d48ac4c14b167e3eecae9c994d7401bd32acd
coderemon24 <mdemonahmed2021@gmail.com>
2025-02-12 01:07:44 +0600

    client section design done

--- js/app.js
+++ js/app.js
@@ -3,10 +3,47 @@ $('.main-slider').slick({
     dots: false,
     arrows: true,
     infinite: true,
-    speed: 300,
-    autoplay: false,
+    speed: 600,
+    autoplay: true,
     autoplaySpeed: 2000,
     slidesToShow: 1,
     adaptiveHeight: true
 });
+
+$('.clients-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 600,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }   
+      }
+    ]
+});
+  
       
